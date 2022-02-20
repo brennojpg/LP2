c32818bd44abf0dbe8a450ff15ed9cedd38281a9
brennojpg <84109474+brennojpg@users.noreply.github.com>
2022-02-20 20:21:17 -0300

    uploading

--- 07 - 2.2.2/hm.java
+++ 07 - 2.2.2/hm.java
@@ -2,17 +2,18 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class painting {
+class hm {
     public static void main (String[] args) {
         PaintFrame frame = new PaintFrame();
         frame.setVisible(true);
     }
 }
+
 class PaintFrame extends JFrame {
-    Rect r1;
-    Rect r2;
-    Rect r3;
-    Rect r4;
+    Elipse e1;
+    Elipse e2;
+    Elipse e3;
+    Elipse e4;
 
     PaintFrame () {
         this.addWindowListener (
@@ -22,29 +23,29 @@ class PaintFrame extends JFrame {
                 }
             }
         );
-        this.setTitle("colorindo retangulos :D");
+        this.setTitle("colorindo elipses :)");
         this.setSize(500, 500);
-        this.r1 = new Rect(200,200, 100,30);
-        this.r2 = new Rect(225,230, 50,100);
-        this.r3 = new Rect(0,330, 500,60);
-        this.r4 = new Rect(240,120, 20,80);
-        this.getContentPane().setBackground(Color.gray);
+        this.e1 = new Elipse(200,200, 100,30);
+        this.e2 = new Elipse(225,230, 50,100);
+        this.e3 = new Elipse(0,330, 500,60);
+        this.e4 = new Elipse(240,120, 20,80);
+        this.getContentPane().setBackground(Color.black);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
-        this.r1.paint(g);
-        this.r2.paint2(g);
-        this.r3.paint3(g);
-        this.r4.paint4(g);
+        this.e1.paint(g);
+        this.e2.paint2(g);
+        this.e3.paint3(g);
+        this.e4.paint4(g);
     }
 }
 
-class Rect {
+class Elipse {
     int x, y;
     int w, h;
 
-    Rect (int x, int y, int w, int h) {
+    Elipse (int x, int y, int w, int h) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -59,36 +60,36 @@ class Rect {
     void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         
-        g.setColor(new Color(255,20,147));
-        g2d.fillRect(this.x, this.y, this.w, this.h);
+        g.setColor(new Color(255,0,0));
+        g2d.fillOval(this.x, this.y, this.w, this.h);
         g2d.setPaint(Color.green);
-        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.drawOval(this.x,this.y, this.w,this.h);
     }  
 
     void paint2 (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         
-        g.setColor(new Color(75,0,130));
-        g2d.fillRect(this.x, this.y, this.w, this.h);
+        g.setColor(new Color(178,34,34));
+        g2d.fillOval(this.x, this.y, this.w, this.h);
         g2d.setPaint(Color.white);
-        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.drawOval(this.x,this.y, this.w,this.h);
     }
         
     void paint3 (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
             
-        g.setColor(new Color(0,255,255));
-        g2d.fillRect(this.x, this.y, this.w, this.h);
-        g2d.setPaint(Color.black);
-        g2d.drawRect(this.x,this.y, this.w,this.h);    
+        g.setColor(new Color(255,99,71));
+        g2d.fillOval(this.x, this.y, this.w, this.h);
+        g2d.setPaint(Color.blue);
+        g2d.drawOval(this.x,this.y, this.w,this.h);    
     }
 
     void paint4 (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
             
         g.setColor(new Color(139,0,0));
-        g2d.fillRect(this.x, this.y, this.w, this.h);
+        g2d.fillOval(this.x, this.y, this.w, this.h);
         g2d.setPaint(Color.yellow);
-        g2d.drawRect(this.x,this.y, this.w,this.h);    
+        g2d.drawOval(this.x,this.y, this.w,this.h);    
     }
 }
